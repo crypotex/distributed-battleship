614c5c72805dcca4678f2ff302cd60f7f93275c1
Andre Tättar <b02166@ut.ee>
2016-12-21 19:00:32 +0200

    Server listens to alive msgs

--- server/server.py
+++ server/server.py
@@ -1,12 +1,10 @@
 import os
 import sys
-import socket
 import threading
 import logging
 import json
 from argparse import ArgumentParser
 from session import Session
-from client_class import Client
 import pika
 try:
     import common as cm
@@ -14,6 +12,7 @@ except ImportError:
     top_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     sys.path.append(top_path)
     import common as cm
+__author__ = "Andre & Annika"
 
 
 TCP_RECIEVE_BUFFER_SIZE = 1024*1024
@@ -39,21 +38,24 @@ class Server:
         self.to_server = self.connection.channel()
         self.to_server.queue_declare(queue='in')
 
+        self.to_server_connection = self.connection.channel()
+        self.to_server_connection.queue_declare(queue="alive_in")
+
+        self.from_server_alive = self.connection.channel()
+        self.from_server_alive.exchange_declare(exchange='alive_out', type="fanout")
+
+        self.thread1 = threading.Thread(target=self.connection_thread)
+        self.thread1.start()
+
         self.from_server = self.connection.channel()
-        #result = self.from_server.queue_declare(queue='out')
+
         self.from_server.exchange_declare(exchange='out', type='direct')
 
-        #self.from_server.queue_bind(exchange='out', queue=result.method.queue)
+        self.to_server.basic_consume(self.process_message, queue='in', no_ack=True)
 
-        self.to_server.basic_consume(self.run_client_thread, queue='in', no_ack=True)
-
-        #print(' [*] Waiting for messages. To exit press CTRL+C')
         self.to_server.start_consuming()
 
-        #self.socket = self.__socket_init(addr, port)
-        #self.main_threader()
-
-    def run_client_thread(self, ch, method, properties, body):
+    def process_message(self, ch, method, properties, body):
         LOG.info("New CALLBACK initialized with :%s." % str(body))
         resp = self.session.handle_request(body)
         enc_resp = json.loads(resp)
@@ -61,6 +63,13 @@ class Server:
         for client in enc_resp['clients']:
             self.from_server.basic_publish(exchange='out', routing_key=client, body=resp)
             LOG.info("Sent response to %s, msg was: %s." % (client, enc_resp))
+
+    def connection_thread(self):
+        self.to_server_connection.basic_consume(self.process_alive_queue_msg, queue="alive_in", no_ack=True)
+        self.to_server_connection.start_consuming()
+
+    def process_alive_queue_msg(self, ch, method, properties, body):
+        print(body)
 
 if __name__ == "__main__":
     # Parsing arguments
